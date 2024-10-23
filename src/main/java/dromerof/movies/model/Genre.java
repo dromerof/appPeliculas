fbc1fd0c0c1113794fe0a22df1357f1adf407d7c
Daniel Romero <dandres508@gmail.com>
2024-10-23 15:33:44 -0500

    Modelos de appPelicula

---
+++ src/main/java/dromerof/movies/model/Genre.java
@@ -0,0 +1,6 @@
+package dromerof.movies.model;
+
+
+public enum Genre {
+    ACTION, COMEDY, DRAMA, HORROR, THRILLER
+}
---
+++ src/main/java/dromerof/movies/model/Movie.java
@@ -0,0 +1,21 @@
+package dromerof.movies.model;
+
+public class Movie {
+
+    private Integer id;
+    private String name;
+    private int minutes;
+    private Genre genre;
+
+    public Movie(String name, int minutes, Genre genre) {
+        // this: call the second constructor.
+        this(null, name, minutes, genre);
+    }
+
+    public Movie(Integer id, String name, int minutes, Genre genre) {
+        this.id = id;
+        this.name = name;
+        this.minutes = minutes;
+        this.genre = genre;
+    }
+}
